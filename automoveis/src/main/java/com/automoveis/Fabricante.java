55ec5773823fb48576ed4f340805ccfbd59972b4
HenriquexxD <henrique_exe@yahoo.com.br>
2023-10-05 21:25:56 -0300

    oiii

--- automoveis/src/main/java/com/automoveis/Fabricante.java
+++ automoveis/src/main/java/com/automoveis/Fabricante.java
@@ -6,5 +6,26 @@ import jakarta.persistence.Table;
 @Entity
 @Table
 public class Fabricante {
+    long id;
+    String nacionalidade;
+    String marca;
     
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 }
